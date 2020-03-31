3d67cc979c0d862dd1fc94cc4546ffc8538cced2
Harsha R <rharshar99@gmail.com>
2020-03-31 12:19:34 +0530

    Add files via upload

---
+++ quicksort.py
@@ -0,0 +1,25 @@
+import time
+def partition(arr,low,high): 
+    i = ( low-1 )         
+    pivot = arr[high]  
+    for j in range(low , high):   
+        if   arr[j] < pivot: 
+            i = i+1 
+            arr[i],arr[j] = arr[j],arr[i] 
+  
+    arr[i+1],arr[high] = arr[high],arr[i+1] 
+    return ( i+1 ) 
+
+def quickSort(arr,low,high): 
+    if low < high: 
+        pi = partition(arr,low,high)
+        quickSort(arr, low, pi-1) 
+        quickSort(arr, pi+1, high)
+
+arr = [10, 7, 8, 9, 1, 5] 
+n = len(arr) 
+start = time.time()
+quickSort(arr,0,n-1) 
+end = time.time()
+print ("Sorted array is:",arr)
+print("time taken is :",(end - start )) 
