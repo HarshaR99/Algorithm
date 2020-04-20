fee4bd4ea69ce498d7e5536df8c2de80efa0a902
Harsha R <rharshar99@gmail.com>
2020-04-20 11:55:53 +0530

    Add files via upload

--- quicksort.py
+++ quicksort.py
@@ -1,25 +1,23 @@
-import time
-def partition(arr,low,high): 
-    i = ( low-1 )         
-    pivot = arr[high]  
-    for j in range(low , high):   
-        if   arr[j] < pivot: 
-            i = i+1 
-            arr[i],arr[j] = arr[j],arr[i] 
-  
-    arr[i+1],arr[high] = arr[high],arr[i+1] 
-    return ( i+1 ) 
+def partition(arr,low,high):
+	pivot = arr[high-1]
+	pindex = low
+	for i in range(low,high-1):
+		if arr[i] < pivot:
+			arr[i], arr[pindex] = arr[pindex], arr[i]
+			# print(True)
+			pindex += 1
+	arr[pindex],arr[high-1] = arr[high-1], arr[pindex]
+	return pindex
 
-def quickSort(arr,low,high): 
-    if low < high: 
-        pi = partition(arr,low,high)
-        quickSort(arr, low, pi-1) 
-        quickSort(arr, pi+1, high)
+def quicksort(arr,low,high):
+	if low < high:
+		pivot = partition(arr,low,high)
+		quicksort(arr,low,pivot-1)
+		quicksort(arr,pivot+1,high)
 
-arr = [10, 7, 8, 9, 1, 5] 
-n = len(arr) 
-start = time.time()
-quickSort(arr,0,n-1) 
-end = time.time()
-print ("Sorted array is:",arr)
-print("time taken is :",(end - start )) 
+
+# driver code
+arr = [2,1,7,6,8,5,3,4]
+print('input array: ',arr)
+quicksort(arr,0,len(arr))
+print('sorterd array :',arr)
